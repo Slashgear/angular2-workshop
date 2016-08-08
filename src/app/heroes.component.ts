499122ec7052e0a9ef4325776ea0f2b26eedfa98
Slashgear <antoine395.caron@gmail.com>
2016-08-08 14:33:16 +0200

    Add loop of heroes

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -3,9 +3,14 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'heroes',
   template: `
-    <h1>Heroes</h1>
+  <h1>Heroes</h1>
+  <ul>
+    <li *ngFor="let name of names">
+      {{name}}
+    </li>
+  </ul>
   `
 })
 export class Heroes {
-
+  names: string[] = ['Ironman', 'Captain America'];
 }
